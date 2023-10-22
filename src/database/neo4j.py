d647c7d40c0282fbe900b6302c2484188f5e394e
Ctibor Kovalčík <ctibor.kovalcik@gmail.com>
2023-10-22 23:32:19 +0200

    ♻️ Singleton to database and logger

--- src/database/neo4j.py
+++ src/database/neo4j.py
@@ -1,24 +1,30 @@
 from neo4j import GraphDatabase
 
 class Neo4jDatabase:
-    def __init__(self, uri, user, password):
+    _instance = None
+    
+    def __new__(cls, uri, user, password):
+        if cls._instance is None:
+            cls._instance = super(Neo4jDatabase, cls).__new__(cls)
+            cls._instance.init_database(uri, user, password)
+        return cls._instance
+
+    def get_instance():
+        return Neo4jDatabase._instance
+
+    def init_database(self, uri, user, password):
         self.driver = GraphDatabase.driver(uri, auth=(user, password))
 
     def close(self):
         self.driver.close()
 
-    def create_node(self, label, properties):
-        node = self.graph.nodes.create(label, **properties)
-        return node
-
-    def print_greeting(self, message):
+    def reset_database(self):
         with self.driver.session() as session:
-            greeting = session.execute_write(self._create_and_return_greeting, message)
-            print(greeting)
-    
+            # Delete all nodes, relationships, and property keys
+            query = "MATCH (n) DETACH DELETE n"
+            session.write_transaction(self._run_query, query)
+
     @staticmethod
-    def _create_and_return_greeting(tx, message):
-        result = tx.run("CREATE (a:Greeting) "
-                        "SET a.message = $message "
-                        "RETURN a.message + ', from node ' + id(a)", message=message)
-        return result.single()[0]
+    def _run_query(tx, query):
+        result = tx.run(query)
+        return result
--- src/logs/logger.py
+++ src/logs/logger.py
@@ -1,8 +1,29 @@
-import logging
+import logging  
+import json
 
 class DataProcessingLogger:
-    def __init__(self, log_file):
+    _instance = None
+    log_file = None
+
+    def __new__(cls, log_file):
+        if cls._instance is None:
+            cls._instance = super(DataProcessingLogger, cls).__new__(cls)
+            cls._instance.init_logger(log_file)
+        return cls._instance
+
+    def init_logger(self, log_file):
+        self.log_file = log_file
         logging.basicConfig(filename=log_file, level=logging.INFO)
 
+    def get_instance():
+        return DataProcessingLogger._instance
+
     def log(self, message):
-        logging.info(message)
+        log_file = "src/logs/indexer_data/indexer.log"
+        with open(log_file, 'a') as file:
+            file.write(json.dumps(message) + '\n')
+
+    def clear(self):
+        log_file = "src/logs/indexer_data/indexer.log"
+        with open(log_file, 'w') as file:
+            file.write('')
