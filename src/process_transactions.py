d7a9694a7f6712be584ef50773ee9b8fc99581d7
Ctibor Kovalčík <ctibor.kovalcik@gmail.com>
2024-03-10 18:43:47 +0100

    ♻️ little refactor + formatter

--- src/process_transactions.py
+++ src/process_transactions.py
@@ -19,19 +19,34 @@ def process_data(start_block, block_count, tx_limit):
         transfers = indexer_service.fetch_transfers(
             start_block, start_block + block_count, 0, tx_limit
         )
-        print(f"Transfers: {len(transfers)}")
 
         if len(transfers) == 0:
+            print("\n\n")
+            print("_" * 50)
+            print(f"Processing {block_count} blocks")
+            print(
+                f"\nBlocks:                                 {start_block} - {start_block + block_count}"
+            )
+            print("Nothing to process")
+            print("¯" * 50)
             start_block += block_count
             continue
 
         repo.save(transfers)
         total_start = time.time()
-        print(f"Blocks: {start_block} - {start_block + block_count}")
+        print("\n\n")
+        print("_" * 50)
+        print(f"Processing {block_count} blocks")
+        print(
+            f"\nBlocks:                                 {start_block} - {start_block + block_count}"
+        )
 
+        # Add new addresses that were created in this block range
         process_users(
             start_block, block_count, tx_limit, indexer_service, transaction_service
         )
+
+        # Add new Tokens that were created in this block range
         process_tokens(
             start_block,
             block_count,
@@ -52,23 +67,29 @@ def process_data(start_block, block_count, tx_limit):
 
         start_block += block_count
         total_end = time.time()
-        print(f"Finished processing in {total_end - total_start} seconds")
+        total_time = "{:.2f}".format(total_end - total_start)
+        print(
+            f"\nFinished processing {block_count} blocks  in            {total_time} seconds"
+        )
+        print("¯" * 50)
 
 
 def process_users(
     start_block, block_count, tx_limit, indexer_service, transaction_service
 ):
-    print("Fetching users...")
     offset_users = 0
+    totalUsers = 0
     while True:
         users = indexer_service.fetch_users(
             start_block, start_block + block_count, offset_users, tx_limit
         )
+        totalUsers += len(users)
         if len(users) == 0:
             break
         transaction_service.insert_addresses(users)
         offset_users += tx_limit
-    print("Users inserted successfully")
+
+    print(f"Unique users processed:                         {totalUsers}")
 
 
 def process_tokens(
@@ -79,17 +100,17 @@ def process_tokens(
     tokenization_service,
     transaction_service,
 ):
-    print("Fetching tokens...")
     offset_tokens = 0
+    total_nfts = 0
     while True:
         nfts = indexer_service.fetch_tokens(
             start_block, start_block + block_count, offset_tokens, tx_limit
         )
+        total_nfts += len(nfts)
         if len(nfts) == 0:
             break
 
         processed_nfts: list[NftWithTags] = []
-        print(f"Tokenizing batch of {len(nfts)} nfts...")
         for nft in nfts:
             tags: list[TagWithValue] = [
                 (tag, value)
@@ -98,27 +119,29 @@ def process_tokens(
             processed_nfts.append((nft, tags))
         transaction_service.insert_nfts(processed_nfts)
         offset_tokens += tx_limit
-    print("Tokens inserted successfully")
+    print(f"Unique tokens processed:                        {total_nfts}")
 
 
 def process_transfers(
     repo, indexer_service, transaction_service, start_block, block_count, tx_limit
 ):
-    print("Processing transfers...")
+    totalTransfers = len(repo.get_transfers())
     offset = 0
     while repo.has_transfers():
         transaction_service.populate_db()
         repo.clear()
         offset += tx_limit
-        repo.save(
-            indexer_service.fetch_transfers(
-                start_block, start_block + block_count, offset, tx_limit
-            )
+        newtransfers = indexer_service.fetch_transfers(
+            start_block, start_block + block_count, offset, tx_limit
         )
+        repo.save(newtransfers)
+        totalTransfers += len(newtransfers)
+
+    print(f"Unique transfers processed:                     {totalTransfers}")
 
 
 def main():
-    start_raw = input("Start block (12337801): ")
+    start_raw = input("Start block (12337801) (blank for 12337801): ")
     start_block = 12337801 if start_raw == "" else int(start_raw)
     block_count = int(input("Block batch count: "))
     tx_limit = int(input("Transaction batch count: "))
@@ -127,4 +150,4 @@ def main():
 
 
 def main_server():
-    process_data(12337801, 100, 100)
+    process_data(0, 1000, 1000)
